e1e47dc61231ee44ac463d96ec5afc3b846a382a
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2025-10-30 07:44:56 +0100

    chore(action): update github action version (#1)
    Refs: #1

--- .ci/build.sh
+++ .ci/build.sh
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 # @author Sebastian Thomschke, https://sebthom.de/
-
+set -x # abort script at first error
 set -e # abort script at first error
 set -o pipefail # causes a pipeline to return the exit status of the last command in the pipe that returned a non-zero return value
 
